72f1cd3b0eda68010055697c49436d7cf4e544cb
mike <mikelka150@gmail.com>
2017-10-07 13:36:33 +0300

    sign in and sign up android ui

--- android/SignLanguageRecognition/app/src/main/java/com/future_prospects/mike/signlanguagerecognition/LoginActivity.java
+++ android/SignLanguageRecognition/app/src/main/java/com/future_prospects/mike/signlanguagerecognition/LoginActivity.java
@@ -3,7 +3,7 @@ package com.future_prospects.mike.signlanguagerecognition;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
